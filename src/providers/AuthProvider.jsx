ba0e8fa5d13f28c0e7ca192f39eb24feddcda039
rohan future devoloper <xrohaislam373@gmail.com>
2023-07-08 22:38:07 +0600

    private Route 1st

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,23 +1,48 @@
-import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
-import app from '../firebase/firebase.config';
+import React, { createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
-    const user = null;
-    
-    const authInfo = {
-        user
-    }
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+   const unSubscribe = onAuthStateChanged(auth, (loggedUser) => {
+      console.log("logged in user inside auth state observed", loggedUser);
+      setUser(loggedUser);
+    });
+    return () =>{
+        unSubscribe();
+    }
+  }, []);
+
+  const authInfo = {
+    user,
+    createUser,
+    signInUser,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
